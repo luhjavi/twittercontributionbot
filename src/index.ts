4009c6eb79e5cae051298abf6d7be7c6bb25e54e
lxFilm <35664082+lxFilm@users.noreply.github.com>
2021-07-17 01:52:54 -0400

    Startup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import { format } from "date-fns";
+import Twitter from "twitter";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const client = new Twitter({
+
+    consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY as string,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
+
+});
