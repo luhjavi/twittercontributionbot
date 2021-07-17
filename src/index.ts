d1390460ddea64336faf194d2134b2da68720408
lxFilm <35664082+lxFilm@users.noreply.github.com>
2021-07-17 02:35:28 -0400

    sssss

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { format } from "date-fns";
 import Twitter from "twitter";
 import fetch from "node-fetch";
+import puppeteet from "puppeteer";
 
 dotenv.config();
 
@@ -13,3 +14,33 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
 
 });
+
+const GITHUHB_URL = "https://github.com/users/madebyFilm/contributions?from=2021-01-01"
+async function grabGithubData(): Promise<string> {
+
+    const browser = await puppeteet.launch();
+    const page = await browser.newPage();
+
+    await page.goto(GITHUHB_URL)
+
+    let contributions = await page.$$eval("[data-count]", (val) =>
+    val.reduce((acc,val)=> acc + +(val.getAttribute("data-count")!), 0)
+    )
+    const currentYear = format(new Date, "yyyy");
+
+    await browser.close();
+
+    return `${currentYear} Github Contributions: ${contributions}`
+}
+
+async function base(){
+    const githubData = await grabGithubData();
+    const url = `https://ixfilm.xyz/discord`
+
+    const params = {
+        location: githubData,
+        url
+    }
+
+    await client.post("account/update_profile")
+}
